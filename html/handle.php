76c0f520876bfab50ba4cb97d846bea64af716da
Yijie Lu <yijielu2@illinois.edu>
2018-10-29 17:28:41 -0500

    php test

--- html/handle.php
+++ html/handle.php
@@ -1,27 +1,50 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="template.css">
+	<title>Result</title>
+</head>
+<body>
 
+<div class="topnav">
+  <a href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+</div>
 
-$result_template = "<!DOCTYPE html>\n
-<html>\n
-<head>\n
-	<title>Result</title>\n
-</head>\n
-<body>\n
-$result
-</body>\n
-</html>\n";
-$url = $_SERVER['REQUEST_URI'];
-$query = parse_url($url, PHP_URL_QUERY);
-parse_str($query, $output);
-// parsed destination, duration, budget for SQL query use
-$destination = $output['destination'];
-$duration = $output['duration'];
-$budget = $output['budget'];
-echo $destination;
-echo "<br>";
-echo $duration;
-echo "<br>";
-echo $budget;
+<!-- <?php
 
+	$url = $_SERVER['REQUEST_URI'];
+	$query = parse_url($url, PHP_URL_QUERY);
+	parse_str($query, $output);
+	// parsed destination, duration, budget for SQL query use
+	$destination = $output['destination'];
+	$duration = $output['duration'];
+	$budget = $output['budget'];
+	echo $destination;
+	echo "<br>";
+	echo $duration;
+	echo "<br>";
+	echo $budget;
 
-?>
+?> -->
+
+<<?php 
+$servername = "localhost";
+$username = "budgetplanner_yijielu2";
+$password = "946983Yy2.2";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+ ?>
+
+</body>
+</html>
